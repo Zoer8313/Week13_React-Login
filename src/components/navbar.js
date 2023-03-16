8f73878acfbf7a7462a7e2c4fa82e3a6927bf88a
Zoe Rothchild <zoer8313@gmail.com>
2023-03-15 21:33:57 -0700

    fixed navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,11 +8,11 @@ export default class Navbar extends React.Component {
                 
                 <nav className = "navbar navbar-expand-lg navbar-dark bg-dark">
                     <a className = "navbar-brand" href = "#">Navbar</a>
-                    <button className = "navbar-toggler" type="button" data-toggle="collapse">
+                    <button className = "navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target = "#navbarNav" aria-controls = "navbarNav">
                     <span className = "navbar-toggler-icon"></span>
                      </button>
 
-                     <div className = "collapse navbar-collapse">
+                     <div className = "collapse navbar-collapse" id = "navbarNav">
                         <ul className = "navbar-nav">
                          <li className = "nav-item">
                       <a className = "nav-link" href = "#">Home</a>
